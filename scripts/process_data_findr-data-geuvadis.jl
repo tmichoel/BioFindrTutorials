990b36e3f3e06100c07b5fa323fb402a9241f8da
tmichoel <11647967+tmichoel@users.noreply.github.com>
2024-06-06 22:33:39 +0200

    update script

--- scripts/process_data_findr-data-geuvadis.jl
+++ scripts/process_data_findr-data-geuvadis.jl
@@ -5,7 +5,7 @@ using DataFrames
 using CSV
 using Arrow
 using Printf
-
+using Statistics
 
 
 #===================
@@ -51,13 +51,95 @@ leftjoin!(enames, gmap, on = :EnsemblID)
 
 # Save data to exp_pro folder
 oname = datadir("exp_pro","findr-data-geuvadis", "dt.arrow")
-Arrow.write(oname,expr)
+Arrow.write(oname,dt)
 
 oname = datadir("exp_pro","findr-data-geuvadis", "dm.arrow")
 Arrow.write(oname,mirna)
 
 oname = datadir("exp_pro","findr-data-geuvadis", "dgt.arrow")
-Arrow.write(oname,geno)
+Arrow.write(oname,dgt)
 
 oname = datadir("exp_pro","findr-data-geuvadis", "dgm.arrow")
 Arrow.write(oname,geno_mirna)
+
+#================
+Rename gene names to HGNC symbols
+=================#
+
+# Load data
+dt = DataFrame(Arrow.Table(datadir("exp_pro","findr-data-geuvadis", "dt.arrow")))
+dgt = DataFrame(Arrow.Table(datadir("exp_pro","findr-data-geuvadis", "dgt.arrow")))
+
+# Load conversion
+dconv = DataFrame(CSV.File(datadir("exp_raw","findr-data-geuvadis","HGNC_ensembl_conversion.txt")))
+
+# create a dictionary to map ensembl ids to approved symbols
+map_names = Dict(zip(dconv[!,:"Ensembl gene ID"], dconv[!,:"Approved symbol"]))
+
+# rename the columns
+new_names = [get(map_names, x, x) for x in names(dt)]
+rename!(dt, new_names)
+
+# find the indices of the genes that were not renamed
+idx = findall(x -> !∈(x,dconv.:"Approved symbol"), new_names)
+
+# create the matching indices for dgt
+idx_dgt = idx[idx .<= ncol(dgt)]
+
+# remove the genes that were not renamed in dt and dgt
+select!(dt, Not(idx))
+select!(dgt, Not(idx_dgt))
+
+# check the correlation between aligned columns
+cc = [cor(dt[:,i], dgt[:,i]) for i in 1:ncol(dgt)]
+
+# keep only the columns that have a correlation greater than 0.2
+idx = findall(x -> abs(x) < 0.2, cc)
+select!(dt, Not(idx))
+select!(dgt, Not(idx))
+
+#========================
+Lingfei's conversion and gold standard data
+=========================#
+
+# Load conversion
+dconv = DataFrame(CSV.File(datadir("exp_raw","findr-data-geuvadis","conversion.txt")))
+
+# Rename gene names
+map_names = Dict(zip(dconv[!,:"Ensembl Gene ID"], dconv[!,:"Associated Gene Name"]))
+map_names_rev = Dict(zip(dconv[!,:"Associated Gene Name"], dconv[!,:"Ensembl Gene ID"]))
+
+# split the ensembl id to remove the version number
+new_names = map(x -> split(x, ".")[1], names(dt))
+rename!(dt, new_names)
+
+# replace ensembl id by gene name if possible
+new_names = [get(map_names, x, x) for x in new_names]
+
+# read gold standard data files
+encode = DataFrame(CSV.File(datadir("exp_raw", "findr-data-geuvadis", "gold", "encode.csv"), header=false))
+sirna = DataFrame(CSV.File(datadir("exp_raw", "findr-data-geuvadis", "gold", "sirna.csv")))
+mirlab = DataFrame(CSV.File(datadir("exp_raw", "findr-data-geuvadis", "gold", "mirlab.csv")))
+
+# get enembl ids from sirna names
+TF_sirna = map(x -> split(x, "_")[2], names(sirna)[4:end])
+intersect!(TF_sirna, names(dt))
+
+# get encode TFs
+TF_encode_names = unique(encode.Column1)
+TF_encode = [get(map_names_rev, x, x) for x in TF_encode_names]
+intersect!(TF_encode, names(dt))
+
+# all TFs
+TF = union(TF_sirna, TF_encode)
+
+# save TFs to file
+oname = datadir("exp_pro","findr-data-geuvadis", "TFs.csv")
+CSV.write(oname, DataFrame(TF=TF))
+
+# mircoRNAs in mirlab
+miRNAs = intersect(unique(mirlab.miRNA), names(dm))
+
+# save miRNAs to file
+oname = datadir("exp_pro","findr-data-geuvadis", "miRNAs.csv")
+CSV.write(oname, DataFrame(miRNA=miRNAs))
